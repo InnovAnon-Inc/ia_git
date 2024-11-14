028c26327ac110cd6da1dc244c50a740cbdd4910
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2024-11-14 07:15:33 -0600

    Initial Commit

--- ia_git/main.pyx
+++ ia_git/main.pyx
@@ -117,7 +117,7 @@ def main()->None:
 	#if ('main' not in local_repo.heads):
 	#local_repo.heads.main.set_tracking_branch(origin.refs.main)
 	#local_repo.heads['main'].set_tracking_branch(origin.refs.main)
-	local_repo.create_head('main', origin.refs.master)
+	local_repo.create_head('main', origin.refs.main)
 	rem_ref:RemoteReference = RemoteReference(local_repo, 'refs/remotes/origin/main')
 	local_repo.head.reference.set_tracking_branch(rem_ref)
 	logger.info('setup tracking')
